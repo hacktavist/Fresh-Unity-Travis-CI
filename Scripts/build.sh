036b4d014a98bd56c02f1f660b5063f4967e3ad5
hacktavist <hacktavist616@gmail.com>
2016-08-16 03:51:26 -0400

    setup travis; setup build scripts

---
+++ Scripts/build.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+# Change this the name of your project. This will be the name of the final executables as well.
+project="TestingTravis"
+
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -quit
+
+
+echo 'Logs from build'
+cat $(pwd)/unity.log
+
+
